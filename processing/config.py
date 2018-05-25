1e0e47749e5fcb2488b26c1946dc4776ae8e0bdb
Will Coughlin <willcoughlin@gmail.com>
2018-05-24 19:26:50 -0500

    create processing logs directory if it does not already exist

--- processing/config.py
+++ processing/config.py
@@ -5,7 +5,12 @@ cloudant_credentials_File = 'credentials/cloudant.credentials.json'
 tweepy_credentials_file = 'credentials/tweepy.credentials.json'
 
 def configure_logging(logfile):
+    import os
     import logging
+
+    if not os.path.isdir('./logs'):
+        os.mkdir('./logs')
+
     logging.basicConfig(      
         format='[%(asctime)s] %(message)s', 
         level=logging.INFO,
